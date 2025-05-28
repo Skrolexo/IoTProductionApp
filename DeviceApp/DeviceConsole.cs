49293b7c2aed06104e5627abb3ae708fc01823a0
Skrolexo <rafal.skolimowski127@gmail.com>
2025-05-28 14:44:14 +0200

    Init commit, creating console app DeviceApp (DeviceConsole and DeviceProduction

---
+++ DeviceApp/DeviceConsole.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Azure.Devices.Client;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using DeviceApp;
+using Microsoft.Azure.Devices;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+public class DeviceConsole
+{
+    public static async Task Main(string[] args)
+    {
+        int index = 0;
+
+        var iotConnections = new List<string>
+        {
+ 
+            ResourcesDevice.Device1ConnectionString,
+            ResourcesDevice.Device2ConnectionString,
+            ResourcesDevice.Device3ConnectionString
+        };
+    }
+}
---
+++ DeviceApp/DeviceProduction.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Communication.Email;
+using Microsoft.Azure.Devices.Client;
+using Opc.UaFx.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceApp
+{
+    internal class DeviceProduction
+    {
+        private readonly DeviceClient _client;
+        private readonly string _uaDeviceName;
+        private readonly OpcClient _opcClient;
+        private int _statusCache = 0;
+
+        private readonly string _mailConnStr = ResourcesDevice.EmailConnectionString;
+        private readonly EmailClient _emailClient;
+        private readonly string _fromEmail = ResourcesDevice.senderEmail;
+        private readonly string _toEmail = ResourcesDevice.receiverEmail;
+
+        [Flags]
+        public enum DeviceStatus
+        {
+            None = 0,
+            EmergencyStop = 1,
+            PowerFailure = 2,
+            SensorFailure = 4,
+            Unknown = 8
+        }
+
+        public DeviceProduction(DeviceClient client, string deviceName, OpcClient opcClient)
+        {
+            _client = client;
+            _uaDeviceName = deviceName;
+            _opcClient = opcClient;
+            _emailClient = new EmailClient(_mailConnStr);
+        }
+    }
+}
