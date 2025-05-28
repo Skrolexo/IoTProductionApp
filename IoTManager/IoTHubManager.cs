2036a9f25d2b56a629f00c7faa67fff652e77cea
Wegles <kuba2255kuba@gmail.com>
2025-05-28 23:25:31 +0200

    Business logic for KPI calculations and error queuing

--- IoTManager/IoTHubManager.cs
+++ IoTManager/IoTHubManager.cs
@@ -6,12 +6,44 @@ namespace IoTManager
 {
     internal class IoTHubManager
     {
-
         private readonly RegistryManager _deviceRegistry;
 
         public IoTHubManager(RegistryManager regManager)
         {
             _deviceRegistry = regManager;
+        }
+
+        public async Task LowerProductionRateAsync(string deviceId)
+        {
+            var twin = await _deviceRegistry.GetTwinAsync(deviceId);
+
+            if (twin == null)
+            {
+                Console.WriteLine($"[WARN] Device Twin for '{deviceId}' not found!");
+                return;
+            }
+
+            var reportedRateObj = twin.Properties.Reported["ProductionRate"];
+            if (reportedRateObj == null)
+            {
+                Console.WriteLine($"[WARN] Reported ProductionRate not set for '{deviceId}'!");
+                return;
+            }
+
+            var reportedRate = (int)reportedRateObj;
+            if (reportedRate >= 10)
+            {
+                twin.Properties.Desired["ProductionRate"] = reportedRate - 10;
+                await _deviceRegistry.UpdateTwinAsync(twin.DeviceId, twin, twin.ETag);
+            }
+        }
+
+
+        public async Task SetEmergencyFlagAsync(string deviceId, int errorCount)
+        {
+            var twin = await _deviceRegistry.GetTwinAsync(deviceId);
+            twin.Properties.Desired["EmergencyTrigger"] = (errorCount >= 3) ? 1 : 0;
+            await _deviceRegistry.UpdateTwinAsync(twin.DeviceId, twin, twin.ETag);
         }
     }
 }
--- IoTManager/ManagerConsole.cs
+++ IoTManager/ManagerConsole.cs
@@ -9,7 +9,7 @@ var serviceBusConn = ResourcesManager.serviceBusConnectionString;
 
 using var devRegistry = RegistryManager.CreateFromConnectionString(iotHubConn);
 
-var iotHubManager = new IoTHubManager( devRegistry);
+var iotHubManager = new IoTHubManager(devRegistry);
 
 await using var busClient = new ServiceBusClient(serviceBusConn);
 await using var kpiQueue = busClient.CreateProcessor(ResourcesManager.KPIQueueName);
@@ -37,15 +37,33 @@ System.Console.WriteLine(">>> Zakończono nasłuch.");
 
 async Task KpiMessageHandler(ProcessMessageEventArgs evt)
 {
-   
+    System.Console.WriteLine($"[KPI QUEUE] Nowa wiadomość:\n\t{evt.Message.Body}");
+
+    var kpiMsg = JObject.Parse(evt.Message.Body.ToString());
+    string devId = kpiMsg["ConnectionDeviceId"].ToString();
+    double kpiVal = kpiMsg["KPI"].Value<double>();
+
+    if (kpiVal < 90)
+        await iotHubManager.LowerProductionRateAsync(devId);
+
+    await evt.CompleteMessageAsync(evt.Message);
 }
 
 async Task ErrorMsgHandler(ProcessMessageEventArgs evt)
 {
-   
+    System.Console.WriteLine($"[ERROR QUEUE] Nowa wiadomość:\n\t{evt.Message.Body}");
+
+    var errMsg = JObject.Parse(evt.Message.Body.ToString());
+    string devId = errMsg["ConnectionDeviceId"].ToString();
+    int sumErr = errMsg["sumErrors"].Value<int>();
+
+    await iotHubManager.SetEmergencyFlagAsync(devId, sumErr);
+
+    await evt.CompleteMessageAsync(evt.Message);
 }
 
 Task CommonErrorHandler(ProcessErrorEventArgs evt)
 {
-    
+    System.Console.WriteLine("[!] ServiceBus BŁĄD: " + evt.Exception.Message);
+    return Task.CompletedTask;
 }
